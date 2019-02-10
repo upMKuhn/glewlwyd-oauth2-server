9e97ee3dc953b0854de168aa38879f33d5aa40ec
Martin Kuhn <martin.kuhn@trivago.com>
2019-02-10 01:04:47 +0100

    auto generate exparam key

--- alpine/entrypoint.sh
+++ alpine/entrypoint.sh
@@ -9,6 +9,18 @@ then
     echo "More information at https://github.com/rafaelhdr/glewlwyd-oauth2-server#volumes"
 fi
 
+if ! [ -f "/var/glewlwyd/keys" ]
+then
+    echo "Mount a volume with file at /var/glewlwyd/keys"
+    exit 1
+fi
+
+if ! [ -f "/var/glewlwyd/keys/ecdsa.pem" ]
+then
+    openssl ecparam -name sect571r1 -genkey -noout -out /var/glewlwyd/keys/ecdsa.pem
+    openssl ec -in /var/glewlwyd/keys/ecdsa.pem -pubout -out /var/glewlwyd/keys/ecdsa.key
+fi
+
 # Run application
 /usr/bin/glewlwyd --config-file=/var/glewlwyd/conf/glewlwyd.conf
 
